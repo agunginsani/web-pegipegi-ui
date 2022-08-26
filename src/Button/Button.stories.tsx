da908ed6a816e183ab71e54ea7c154d6a78a5328
Agung Insani <agunginsanialam@gmail.com>
2022-08-26 16:21:25 +0700

    feat: add tailwind

---
+++ src/Button/Button.stories.tsx
@@ -0,0 +1,34 @@
+import Button from './Button.vue';
+import { Meta, StoryFn } from '@storybook/vue3';
+
+export default {
+  title: 'Button',
+  component: Button,
+  parameters: { actions: { argTypesRegex: '^on.*' } },
+  argTypes: {
+    variant: {
+      control: 'radio',
+      options: ['filled', 'outline', 'text'],
+      defaultValue: 'filled',
+    },
+    size: {
+      control: 'radio',
+      options: ['small', 'medium', 'large'],
+      defaultValue: 'medium',
+    },
+    disabled: {
+      control: 'boolean',
+      defaultValue: false,
+    },
+  },
+} as Meta<typeof Button>;
+
+export const Template: StoryFn<typeof Button> = (args) => ({
+  components: { Button },
+  setup() {
+    return { args };
+  },
+  template: '<Button v-bind="args">Button</Button>',
+});
+
+Template.storyName = 'Button';
