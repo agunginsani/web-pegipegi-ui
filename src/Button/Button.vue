eab67aba41d5a234489a647c590b19e8b9964a7a
Agung Insani <agunginsanialam@gmail.com>
2022-08-29 15:32:13 +0700

    refactor: update button type

--- src/Button/Button.vue
+++ src/Button/Button.vue
@@ -10,28 +10,33 @@
   type ButtonProps = {
     variant?: "filled" | "outline" | "text";
     size?: "small" | "medium" | "large";
+    disabled?: boolean;
   };
 
   withDefaults(defineProps<ButtonProps>(), {
     variant: "filled",
     size: "medium",
+    disabled: false,
   });
 </script>
 
 <template>
   <button
-    class="border font-bold disabled:text-platinum-400"
-    :class="{
-      'bg-orange-400 hover:bg-orange-500 border-orange-400 hover:border-orange-500 text-white disabled:bg-platinum-50 disabled:border-platinum-50':
-        variant === 'filled',
-      'bg-transparent text-orange-400 border-orange-400 hover:border-orange-500 hover:text-orange-500 disabled:border-gray-50 disabled:text-platinum-400':
-        variant === 'outline',
-      'border-transparent hover:bg-orange-25 disabled:bg-transparent text-orange-400':
-        variant === 'text',
-      'py-2 px-4 text-sm rounded-[18px]': size === 'small',
-      'py-3 px-6 text-sm rounded-[22px]': size === 'medium',
-      'py-4 px-8 text-md rounded-[28px]': size === 'large',
-    }"
+    :class="[
+      'border font-bold disabled:text-platinum-400',
+      {
+        'bg-orange-400 hover:bg-orange-500 border-orange-400 hover:border-orange-500 text-white disabled:bg-platinum-50 disabled:border-platinum-50':
+          variant === 'filled',
+        'bg-transparent text-orange-400 border-orange-400 hover:border-orange-500 hover:text-orange-500 disabled:border-gray-50 disabled:text-platinum-400':
+          variant === 'outline',
+        'border-transparent hover:bg-orange-25 disabled:bg-transparent text-orange-400':
+          variant === 'text',
+        'py-2 px-4 text-sm rounded-[18px]': size === 'small',
+        'py-3 px-6 text-sm rounded-[22px]': size === 'medium',
+        'py-4 px-8 text-md rounded-[28px]': size === 'large',
+      },
+    ]"
+    :disabled="disabled"
   >
     <slot />
   </button>
