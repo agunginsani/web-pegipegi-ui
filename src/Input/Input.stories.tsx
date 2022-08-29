e4a2769bcb601c6e15d12e78c9d0b939a6d66c04
Agung Insani <agunginsanialam@gmail.com>
2022-08-29 15:16:20 +0700

    feat: add input component

---
+++ src/Input/Input.stories.tsx
@@ -0,0 +1,32 @@
+import Input from "./Input.vue";
+import { Meta, StoryFn } from "@storybook/vue3";
+
+export default {
+  title: "Input",
+  component: Input,
+  argTypes: {
+    size: {
+      control: "radio",
+      options: ["small", "medium", "large"],
+      defaultValue: "medium",
+    },
+    disabled: {
+      control: "boolean",
+      defaultValue: false,
+    },
+    error: {
+      control: "boolean",
+      defaultValue: false,
+    },
+  },
+} as Meta<typeof Input>;
+
+export const Template: StoryFn<typeof Input> = (args) => ({
+  components: { Input },
+  setup() {
+    return { args };
+  },
+  template: '<Input v-bind="args" />',
+});
+
+Template.storyName = "Input";
---
+++ src/Input/Input.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { defineComponent } from "vue";
+
+  defineComponent({
+    name: "PInput",
+  });
+</script>
+
+<script lang="ts" setup>
+  type InputProps = {
+    size?: "small" | "medium" | "large";
+    error?: boolean;
+    disabled?: boolean;
+  };
+
+  withDefaults(defineProps<InputProps>(), {
+    size: "medium",
+    error: false,
+    disabled: false,
+  });
+</script>
+
+<template>
+  <input
+    :class="[
+      'border rounded-lg rounded-tl-none outline-none',
+      'focus:border-orange-400',
+      'disabled:bg-platinum-50',
+      {
+        'px-3 py-2': size === 'small',
+        'p-3': size === 'medium',
+        'p-4': size === 'large',
+      },
+      !disabled && error
+        ? 'bg-red-25 border-red-400 text-red-400'
+        : 'bg-transparent border-gray-50 text-gray-900',
+    ]"
+    :disabled="disabled"
+  />
+</template>
