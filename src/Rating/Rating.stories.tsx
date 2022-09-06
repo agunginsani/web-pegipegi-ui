52c520fb9b38491af21744e4d805617b9857e578
willywnt <willywinata1688@gmail.com>
2022-09-06 17:47:00 +0700

    chore: change disabled to readOnly and adjust style

--- src/Rating/Rating.stories.tsx
+++ src/Rating/Rating.stories.tsx
@@ -19,7 +19,7 @@ export default {
       options: [0.5, 1],
       defaultValue: 1,
     },
-    disabled: {
+    readOnly: {
       control: "boolean",
       defaultValue: false,
     },
--- src/Rating/Rating.vue
+++ src/Rating/Rating.vue
@@ -11,14 +11,14 @@
     value?: number;
     max?: number;
     precision?: 0.5 | 1;
-    disabled?: boolean;
+    readOnly?: boolean;
   };
 
   const props = withDefaults(defineProps<RatingProps>(), {
     value: 3,
     max: 5,
     precision: 1,
-    disabled: false,
+    readOnly: false,
   });
 
   const emit = defineEmits<{
@@ -29,7 +29,7 @@
     (rating) => (rating + 1) * 0.5
   );
   const rating = ref(props.value);
-  const ratingHover = ref(props.value);
+  const hover = ref(props.value);
   const uid = ref(+new Date());
 
   function onChangeRating(value: number) {
@@ -49,22 +49,21 @@
       <label
         :for="`rating-${uid}-${ratingValue}`"
         :class="[
-          { 'cursor-pointer': !disabled },
-          { 'pointer-events-none': disabled },
+          readOnly ? 'pointer-events-none' : 'cursor-pointer',
+          { 'pr-[16px] last:pr-0': ratingValue % 1 === 0 },
         ]"
         @mouseover="
-          ratingHover =
+          hover =
             precision === 1 && ratingValue % 1 !== 0
               ? ratingValue + 0.5
               : ratingValue
         "
-        @mouseleave="ratingHover = rating"
+        @mouseleave="hover = rating"
       >
         <svg
           :class="[
-            ratingValue <= ratingHover ? 'fill-[#FFB81C]' : 'fill-platinum-200',
-            ratingValue % 1 === 0 ? 'scale-x-[-1]' : '',
-            ratingValue % 1 === 0 && ratingValue !== max ? 'mr-[16px]' : '',
+            ratingValue <= hover ? 'fill-[#FFB81C]' : 'fill-platinum-200',
+            { 'scale-x-[-1]': ratingValue % 1 === 0 },
           ]"
           width="10"
           height="21"
@@ -76,16 +75,16 @@
             d="M6.56808 6.05629L9.09184 0.581319C9.26207 0.212034 9.99104 0.00124223 9.99104 0.00124223L9.99997 17.019L4.92742 19.8716C4.1941 20.284 3.31031 19.6616 3.45142 18.8322L4.47605 12.8098L0.289208 8.57893C-0.287975 7.99568 0.0442224 7.00386 0.856264 6.88591L6.56808 6.05629Z"
           />
         </svg>
+        <input
+          :id="`rating-${uid}-${ratingValue}`"
+          type="radio"
+          name="rating"
+          class="hidden"
+          :value="ratingValue"
+          :checked="ratingValue === value"
+          @change="onChangeRating(ratingValue)"
+        />
       </label>
-      <input
-        :id="`rating-${uid}-${ratingValue}`"
-        type="radio"
-        name="rating"
-        class="hidden"
-        :value="ratingValue"
-        :checked="ratingValue === value"
-        @change="onChangeRating(ratingValue)"
-      />
     </template>
   </fieldset>
 </template>
