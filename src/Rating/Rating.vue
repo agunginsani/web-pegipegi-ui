fe1cfa3fd256c93de7626c0ca0dcb910027f35be
willywnt <willywinata1688@gmail.com>
2022-09-07 14:40:21 +0700

    fix: space white between star

--- src/Rating/Rating.vue
+++ src/Rating/Rating.vue
@@ -41,7 +41,7 @@
     emit("input", rating.value);
   }
 
-  function onRatingHover(value: number) {
+  function handleRatingHover(value: number) {
     hover.value =
       props.precision === 1 && value % 1 !== 0 ? value + 0.5 : value;
   }
@@ -55,21 +55,35 @@
       class="rating flex hover:scale-[1.2]"
       :class="[{ 'pointer-events-none': readOnly }]"
     >
+      <svg
+        class="absolute"
+        :class="[star <= hover ? 'fill-yellow-400' : 'fill-platinum-200']"
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M6.56808 6.05634L9.09184 0.581372C9.44917 -0.193791 10.5508 -0.193791 10.9082 0.581372L13.4319 6.05634L19.1437 6.88596C19.9558 7.00391 20.288 7.99573 19.7108 8.57898L15.5239 12.8099L16.5479 18.8324C16.6889 19.6618 15.8051 20.284 15.0718 19.8716L9.99997 17.019L4.92742 19.8716C4.1941 20.284 3.31031 19.6617 3.45142 18.8323L4.47605 12.8098L0.289208 8.57898C-0.287975 7.99573 0.0442224 7.00391 0.856264 6.88596L6.56808 6.05634Z"
+        />
+      </svg>
+
       <template
         v-for="ratingValue in ratingOptions.slice((star - 1) * 2, star * 2)"
         :key="`rating-${uuid}-${ratingValue}`"
       >
         <label
           :for="`rating-${uuid}-${ratingValue}`"
-          class="cursor-pointer"
+          class="cursor-pointer relative"
           :class="[{ 'last-of-type:pr-[16px]': star !== max }]"
-          @mouseover="onRatingHover(ratingValue)"
+          @mouseover="handleRatingHover(ratingValue)"
           @mouseleave="hover = rating"
         >
           <span class="sr-only">Rating {{ ratingValue }} out of {{ max }}</span>
           <svg
             :class="[
-              ratingValue <= hover ? 'fill-[#FFB81C]' : 'fill-gray-50',
+              ratingValue <= hover ? 'fill-yellow-400' : 'fill-platinum-200',
               { 'scale-x-[-1]': ratingValue % 1 === 0 },
             ]"
             width="10"
