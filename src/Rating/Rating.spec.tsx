0d5b158690d6cdb7a8183526e751439586815a62
Agung Insani <agunginsanialam@gmail.com>
2022-09-11 00:03:31 +0700

    test: support jsx and add unit test

--- src/Rating/Rating.spec.tsx
+++ src/Rating/Rating.spec.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/vue';
 import userEvent from '@testing-library/user-event';
 import Rating from './Rating.vue';
+import { defineComponent, ref } from 'vue';
 
 it.skip('renders and emits correctly when precision is 1', async () => {
   const user = userEvent.setup();
@@ -48,4 +49,37 @@ it('renders and emits correctly when precision is 0.5', async () => {
   expect(emitted('update:modelValue').length).toBe(1);
   expect(emitted('update:modelValue')[0]).toEqual([3.5]);
   expect(ratingThreeAndHalf).toBeChecked();
+});
+
+const ReactiveRating = defineComponent({
+  components: { Rating },
+  setup() {
+    const rating = ref(3);
+
+    function handleAdd() {
+      rating.value += 1;
+    }
+
+    function handleRemove() {
+      rating.value -= 1;
+    }
+
+    return () => (
+      <div>
+        <button onClick={handleAdd}>Add star</button>
+        <button onClick={handleRemove}>Remove star</button>
+        <Rating v-model={rating.value} />
+      </div>
+    );
+  },
+});
+
+it('handles `RactiveRating`', async () => {
+  const user = userEvent.setup();
+  render(ReactiveRating);
+  expect(screen.getByRole('radio', { name: /rating 3 /i })).toBeChecked();
+  await user.click(screen.getByRole('button', { name: /add/i }));
+  expect(screen.getByRole('radio', { name: /rating 4 /i })).toBeChecked();
+  await user.click(screen.getByRole('button', { name: /remove/i }));
+  expect(screen.getByRole('radio', { name: /rating 3 /i })).toBeChecked();
 });
