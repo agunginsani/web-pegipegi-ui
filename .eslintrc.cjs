7d1feb26d4ba8c1fc2fc1d4e6a4bd2c665fb9cbd
Agung Insani <agunginsanialam@gmail.com>
2022-09-18 19:57:01 +0700

    chore: update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,18 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:vue/vue3-recommended",
-    "@vue/typescript/recommended",
-    "@vue/eslint-config-prettier",
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/typescript/recommended',
+    '@vue/eslint-config-prettier',
+  ],
+  overrides: [
+    {
+      files: ['**/*.spec.{ts,tsx}'],
+      rules: {
+        'vue/one-component-per-file': 'off',
+      },
+    },
   ],
   root: true,
 };
