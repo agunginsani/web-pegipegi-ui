fb6d0760860901e3a120a56cd952aedec77a1a29
Redchlorophyll <dhonni.ari@pegipegi.com>
2022-09-21 11:59:12 +0700

    test: add unit test for Switch

---
+++ src/Switch/Switch.spec.tsx
@@ -0,0 +1,40 @@
+import { defineComponent, definedComponent, PropType, ref } from 'vue';
+import { render, screen } from '@testing-library/vue';
+import userEvent from '@testing-library/user-event';
+import Switch from './Switch.vue';
+
+const SwitchWrapper = defineComponent({
+  setup(props, { attrs }) {
+    return () => <Switch {...attrs} />;
+  },
+});
+
+it('handles <Switch />', async () => {
+  const user = userEvent.setup();
+  const props = {
+    onClick: vi.fn(),
+  };
+  render(SwitchWrapper, { props });
+  const switcher = screen.getByRole('checkbox');
+  await user.click(switcher);
+  expect(props.onClick).toHaveBeenCalledTimes(1);
+  expect(switcher).toBeChecked();
+});
+
+it('handles <Switch disabled />', async () => {
+  const user = userEvent.setup();
+  render(SwitchWrapper, { props: { disabled: true } });
+  const switcher = screen.getByRole('checkbox');
+  await user.click(switcher);
+  expect(switcher).toBeDisabled();
+  expect(switcher).not.toBeChecked();
+});
+
+it('handles <Switch disabled modelValue />', async () => {
+  const user = userEvent.setup();
+  render(SwitchWrapper, { props: { disabled: true, modelValue: true } });
+  const switcher = screen.getByRole('checkbox');
+  await user.click(switcher);
+  expect(switcher).toBeDisabled();
+  expect(switcher).toBeChecked();
+});
--- src/Switch/Switch.vue
+++ src/Switch/Switch.vue
@@ -17,6 +17,8 @@
     (e: 'update:modelValue', value: boolean): void;
   };
 
+  const emit = defineEmits<SwitchEmits>();
+
   const props = withDefaults(defineProps<SwitchProps>(), {
     disabled: false,
     modelValue: false,
@@ -31,6 +33,7 @@
       class="peer w-[52px] h-[52px] absolute opacity-0 z-[1]"
       type="checkbox"
       :disabled="props.disabled"
+      @change="emit('update:modelValue', !props.modelValue)"
     />
     <div
       :class="[
@@ -45,7 +48,7 @@
         },
         { 'peer-checked:[&>*]:bg-white': props.os === 'ios' },
         {
-          '[&>*]:bg-[#B3B5C6] bg-[#F6F6F9] peer-checked:[&>*]::bg-[#B3B5C6] peer-checked:bg-[#F6F6F9] ':
+          '[&>*]:bg-[#B3B5C6] bg-[#F6F6F9] peer-checked:[&>*]:bg-[#B3B5C6] peer-checked:bg-[#F6F6F9] ':
             props.disabled && props.os === 'most_device',
         },
       ]"
