c806070ef70f71ad2d3c0301e47a98a4ab70bff4
Muhammad Fiqih Firdiansyah <fiqih.firdiansyah@pegipegi.com>
2022-09-21 12:07:55 +0700

    test: add unit test

---
+++ src/Checkbox/Checkbox.spec.tsx
@@ -0,0 +1,48 @@
+import { render, screen } from '@testing-library/vue';
+import userEvent from '@testing-library/user-event';
+import Checkbox from './Checkbox.vue';
+import { defineComponent, ref, watch } from 'vue';
+
+const initialValue = true;
+
+const ControlledInput = defineComponent({
+  emits: ['valueChanged'],
+  setup(props, { attrs, emit }) {
+    const value = ref(initialValue);
+
+    watch(value, (newValue) => {
+      emit('valueChanged', newValue);
+    });
+
+    return () => (
+      <div>
+        <label for="checkbox">Checkbox</label>
+        <Checkbox id="checkbox" checked={value.value} {...attrs} />
+      </div>
+    );
+  },
+});
+
+it('handles <ControlledInput />', async () => {
+  const user = userEvent.setup();
+  const props = {
+    onValueChanged: vi.fn(),
+    onFocus: vi.fn(),
+    onBlur: vi.fn(),
+    onInput: vi.fn(),
+  };
+
+  // isChecked = true
+  render(ControlledInput, { props });
+  const checkbox = screen.getByRole('checkbox', { name: /checkbox/i });
+  expect(checkbox).toBeChecked()
+
+  // isChecked = false
+  await user.click(checkbox);
+  expect(checkbox).not.toBeChecked()
+});
+
+it('handles <ControlledInput disabled />', () => {
+  render(ControlledInput, { props: { disabled: true } });
+  expect(screen.getByRole('checkbox', { name: /checkbox/i })).toBeDisabled();
+});
