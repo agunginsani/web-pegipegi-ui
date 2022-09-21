091474868b201dafeb891c925f4667a16185d60d
Muhammad Fiqih Firdiansyah <fiqih.firdiansyah@pegipegi.com>
2022-09-21 12:11:23 +0700

    fix: minor

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 export { default as Button } from './Button/Button.vue';
 export { default as Input } from './Input/Input.vue';
 export { default as Rating } from './Rating/Rating.vue';
-export { default as Checkbox } from './Rating/Checkbox.vue';
+export { default as Checkbox } from './Checkbox/Checkbox.vue';
