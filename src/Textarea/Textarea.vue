9bf8961d2fa2c1cf7b5c438f718588f51bbb4b2e
Dwika Setya <dwika.setya@pegipegi.com>
2022-09-26 16:35:03 +0700

    feat: use definecomponent in textarea

--- src/Textarea/Textarea.vue
+++ src/Textarea/Textarea.vue
@@ -1,8 +1,10 @@
 <script lang="ts">
-  export default {
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
     name: 'PTextarea',
     inheritAttrs: false,
-  };
+  });
 </script>
 
 <script lang="ts" setup>
