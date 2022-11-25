ba22448910dc90a35cb461ffd9928a48a7db5800
Agung Insani <agunginsanialam@gmail.com>
2022-11-25 23:32:36 +0700

    refactor: remove component-style

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-import './main.css';
-
 export { default as Button } from './Button/Button.vue';
 export { default as Input } from './Input/Input.vue';
 export { default as Rating } from './Rating/Rating.vue';
