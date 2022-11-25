ed462a4075e1577f6b4e300db64bdc634e30cc41
Agung Insani <agunginsanialam@gmail.com>
2022-11-25 21:59:44 +0700

    refactor: simplify usage

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import './main.css';
+
 export { default as Button } from './Button/Button.vue';
 export { default as Input } from './Input/Input.vue';
 export { default as Rating } from './Rating/Rating.vue';
