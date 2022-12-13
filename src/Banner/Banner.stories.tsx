25f4adc826c92e708225438a74b35048d3d7fb83
Fauzan Khalif <fauzan.khalif@pegipegi.com>
2022-12-13 11:20:12 +0700

    chore: change tabbing

--- src/Banner/Banner.stories.tsx
+++ src/Banner/Banner.stories.tsx
@@ -8,73 +8,73 @@ export default {
 export const Template: StoryFn<typeof Banner> = () => ({
   components: { Banner },
   template: `
-              <div class="flex gap-x-5">
-                <div class="grid gap-y-2">
-                  <Banner color="neutral" priority="high">
-                    <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
-                    <template #title><strong>Title Message</strong></template>
-                    <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
-                  </Banner>
-                  <Banner color="accent" priority="high">
-                    <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
-                    <template #title><strong>Title Message</strong></template>
-                    <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
-                  </Banner>
-                  <Banner color="info" priority="high">
-                    <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
-                    <template #title><strong>Title Message</strong></template>
-                    <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
-                  </Banner>
-                  <Banner color="warning" priority="high">
-                    <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
-                    <template #title><strong>Title Message</strong></template>
-                    <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
-                  </Banner>
-                  <Banner color="positive" priority="high">
-                    <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
-                    <template #title><strong>Title Message</strong></template>
-                    <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
-                  </Banner>
-                  <Banner color="negative" priority="high">
-                    <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
-                    <template #title><strong>Title Message</strong></template>
-                    <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
-                  </Banner>
-                </div>
-                <div class="grid gap-y-2"> 
-                  <Banner color="neutral" priority="low">
-                    <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
-                    <template #title><strong>Title Message</strong></template>
-                    <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
-                  </Banner>
-                  <Banner color="accent" priority="low">
-                    <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
-                    <template #title><strong>Title Message</strong></template>
-                    <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
-                  </Banner>
-                  <Banner color="info" priority="low">
-                    <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
-                    <template #title><strong>Title Message</strong></template>
-                    <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
-                  </Banner>
-                  <Banner color="warning" priority="low">
-                    <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
-                    <template #title><strong>Title Message</strong></template>
-                    <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
-                  </Banner>
-                  <Banner color="positive" priority="low">
-                    <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
-                    <template #title><strong>Title Message</strong></template>
-                    <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
-                  </Banner>
-                  <Banner color="negative" priority="low">
-                    <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
-                    <template #title><strong>Title Message</strong></template>
-                    <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
-                  </Banner>
-                </div>
-              </div>
-            `,
+    <div class="flex gap-x-5">
+      <div class="grid gap-y-2">
+        <Banner color="neutral" priority="high">
+          <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
+          <template #title><strong>Title Message</strong></template>
+          <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
+        </Banner>
+        <Banner color="accent" priority="high">
+          <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
+          <template #title><strong>Title Message</strong></template>
+          <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
+        </Banner>
+        <Banner color="info" priority="high">
+          <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
+          <template #title><strong>Title Message</strong></template>
+          <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
+        </Banner>
+        <Banner color="warning" priority="high">
+          <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
+          <template #title><strong>Title Message</strong></template>
+          <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
+        </Banner>
+        <Banner color="positive" priority="high">
+          <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
+          <template #title><strong>Title Message</strong></template>
+          <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
+        </Banner>
+        <Banner color="negative" priority="high">
+          <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
+          <template #title><strong>Title Message</strong></template>
+          <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
+        </Banner>
+      </div>
+      <div class="grid gap-y-2"> 
+        <Banner color="neutral" priority="low">
+          <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
+          <template #title><strong>Title Message</strong></template>
+          <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
+        </Banner>
+        <Banner color="accent" priority="low">
+          <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
+          <template #title><strong>Title Message</strong></template>
+          <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
+        </Banner>
+        <Banner color="info" priority="low">
+          <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
+          <template #title><strong>Title Message</strong></template>
+          <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
+        </Banner>
+        <Banner color="warning" priority="low">
+          <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
+          <template #title><strong>Title Message</strong></template>
+          <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
+        </Banner>
+        <Banner color="positive" priority="low">
+          <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
+          <template #title><strong>Title Message</strong></template>
+          <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
+        </Banner>
+        <Banner color="negative" priority="low">
+          <template #icon><img width="14" height="14" src="./src/Banner/logo.svg" alt="logo"/></template>
+          <template #title><strong>Title Message</strong></template>
+          <template #description><p>Write a short message with a maximum of only 2 lines.</p></template>
+        </Banner>
+      </div>
+    </div>
+  `,
 });
 
 Template.storyName = 'Banner';
